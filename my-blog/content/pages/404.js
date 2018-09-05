e6e08b324311ae0bdfb483c0108a16aac85eadf7
Luciano Mammino <lmammino@vectra.ai>
2018-09-05 09:24:06 +0100

    Completed 404 page

--- my-blog/content/pages/404.js
+++ my-blog/content/pages/404.js
@@ -16,19 +16,37 @@ const NotFoundPage = props => {
       <Helmet title={`PAGE NOT FOUND - ${siteTitle}`} />
       <ResponsiveWrapper>
         <ArticleContainer style={{ minHeight: '80vh' }}>
-          <img
-            src={ToadPicture}
-            style={{ width: '60px', float: 'left', margin: '0 1em 1em 0' }}
-          />
-          <h1 style={{ marginTop: '4em' }}>
-            PAGE NOT FOUND
-            <br />
-            a.k.a. <span style={{ color: '#ce1318' }}>404 Error</span>!
+          <h1 style={{ marginTop: '4em', textAlign: 'center' }}>
+            <span style={{ color: '#ce1318', fontWeight: 'bolder' }}>404 Error</span>
           </h1>
-          <p>
+          <h1 style={{ textAlign: 'center' }}>
+            Thank you for your visit
+            <br />
+            But the page you were looking for is in Another Castle<sup>1</sup>
+          </h1>
+          <p style={{ textAlign: 'center' }}>
             You’re either misspelling the URL or requesting a page that&apos;s
             no longer here.
           </p>
+          <p style={{ textAlign: 'center' }}>
+            <img
+              src={ToadPicture}
+              style={{ width: '60px', margin: '1em' }}
+            />
+          </p>
+          <p style={{ textAlign: 'center' }}>
+            <small>
+              <sup>1</sup>
+              Yes, this was another{' '}
+              <a
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://knowyourmeme.com/memes/but-our-princess-is-in-another-castle">
+                  Super Mario&trade; reference
+              </a>...
+            </small>
+          </p>
+
           <h2>Since you are here...</h2>
           <p>Why don&#39;t you have a look at one of my latest articles?</p>
           <ul>
