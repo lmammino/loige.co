565e9e28c83b7e9e7d3bc070c40765db29bcb993
Luciano Mammino <lucianomammino@gmail.com>
2022-08-22 13:49:24 +0100

    Completed footer

--- my-astro-site/src/site.ts
+++ my-astro-site/src/site.ts
@@ -7,34 +7,39 @@ import GithubIcon from './components/icons/Github.astro'
 const site = {
   startYear: 2014,
   currentYear: new Date().getFullYear(),
-  social:
-    [
-      {
-        name: 'Twitter',
-        href: 'https://twitter.com/loige',
-        Icon: TwitterIcon
-      },
-      {
-        name: 'Twitch',
-        href: 'https://twitch.tv/loige',
-        Icon: TwitchIcon
-      },
-      {
-        name: 'Youtube',
-        href: 'https://www.youtube.com/loige',
-        Icon: YoutubeIcon
-      },
-      {
-        name: 'Linkedin',
-        href: 'https://www.linkedin.com/in/lucianomammino/',
-        Icon: LinkedinIcon
-      },
-      {
-        name: 'Github',
-        href: 'https://github.com/lmammino',
-        Icon: GithubIcon
-      }
-    ]
+  social: [
+    {
+      name: 'Twitter',
+      href: 'https://twitter.com/loige',
+      Icon: TwitterIcon
+    },
+    {
+      name: 'Twitch',
+      href: 'https://twitch.tv/loige',
+      Icon: TwitchIcon
+    },
+    {
+      name: 'Youtube',
+      href: 'https://www.youtube.com/loige',
+      Icon: YoutubeIcon
+    },
+    {
+      name: 'Linkedin',
+      href: 'https://www.linkedin.com/in/lucianomammino/',
+      Icon: LinkedinIcon
+    },
+    {
+      name: 'Github',
+      href: 'https://github.com/lmammino',
+      Icon: GithubIcon
+    }
+  ],
+  projects: [
+    { name: 'Node.js Design Patterns', href: 'https://www.nodejsdesignpatterns.com' },
+    { name: 'AWS Bites', href: 'https://awsbites.com' },
+    { name: 'FullStack Bulletin', href: 'https://fullstackbulletin.com' },
+    { name: 'Middy', href: 'https://middy.js.org' },
+  ]
 }
 
 export default site
