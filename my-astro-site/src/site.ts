80d318e6843196abf9ba804b8114d47f852a3e76
Luciano Mammino <lucianomammino@gmail.com>
2022-08-22 09:55:27 +0100

    public files, some refactoring on layout, started working on footer component

---
+++ my-astro-site/src/site.ts
@@ -0,0 +1,40 @@
+import TwitterIcon from './components/icons/Twitter.astro'
+import TwitchIcon from './components/icons/Twitch.astro'
+import YoutubeIcon from './components/icons/Youtube.astro'
+import LinkedinIcon from './components/icons/Linkedin.astro'
+import GithubIcon from './components/icons/Github.astro'
+
+const site = {
+  startYear: 2014,
+  currentYear: new Date().getFullYear(),
+  social:
+    [
+      {
+        name: 'Twitter',
+        href: 'https://twitter.com/loige',
+        Icon: TwitterIcon
+      },
+      {
+        name: 'Twitch',
+        href: 'https://twitch.tv/loige',
+        Icon: TwitchIcon
+      },
+      {
+        name: 'Youtube',
+        href: 'https://www.youtube.com/loige',
+        Icon: YoutubeIcon
+      },
+      {
+        name: 'Linkedin',
+        href: 'https://www.linkedin.com/in/lucianomammino/',
+        Icon: LinkedinIcon
+      },
+      {
+        name: 'Github',
+        href: 'https://github.com/lmammino',
+        Icon: GithubIcon
+      }
+    ]
+}
+
+export default site
