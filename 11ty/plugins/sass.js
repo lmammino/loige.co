4f8a0f7352d52e78ecb994ea14d2098c249e79ae
Luciano Mammino <lucianomammino@gmail.com>
2022-10-16 17:19:37 +0100

    Simplified template inherithance tree, improved sass plugin

--- 11ty/plugins/sass.js
+++ 11ty/plugins/sass.js
@@ -31,48 +31,68 @@ module.exports = function (eleventyConfig, config = {}) {
   })
 
   eleventyConfig.addAsyncFilter('sass', async function (filePath, ctx) {
-    const sourcePath = path.join(path.dirname(this.ctx.page.inputPath), filePath)
-    if (processed.has(sourcePath)) {
-      return processed.get(sourcePath)
+    try {
+      const loadPaths = [
+        path.dirname(this.ctx.page.inputPath),
+        eleventyConfig.dir.input,
+        path.join(eleventyConfig.dir.input, (eleventyConfig.dir.includes || '_includes')),
+        ...conf.includePaths
+      ]
+      let sourcePath
+      for (const loadPath of loadPaths) {
+        sourcePath = path.join(loadPath, filePath)
+        if (await fs.access(sourcePath).then(() => true).catch(() => false)) {
+          break
+        }
+      }
+
+      if (!sourcePath) {
+        throw new Error(`Sass file not found: ${filePath} (searched in ${loadPaths.join(', ')})`)
+      }
+
+      if (processed.has(sourcePath)) {
+        return processed.get(sourcePath)
+      }
+
+      const result = await sass.compileAsync(sourcePath, {
+        style: conf.minify ? 'compressed' : 'expanded',
+        sourceMap: conf.sourceMap,
+        sourceMapIncludeSources: conf.sourceMapIncludeSources,
+        loadPaths
+      })
+
+      const outputPath = path.join(eleventyConfig.dir.output, conf.outputPath)
+      const fileHash = crypto.createHash('md5').update(result.css).digest('hex').substring(0, 8)
+      const destFileName = `${path.basename(filePath, '.scss')}.${fileHash}.css`
+      const destFileMapName = `${destFileName}.map`
+      const destFilePath = path.join(outputPath, destFileName)
+      const destFileUrl = `/${conf.outputPath}/${destFileName}`
+      const destFileMapPath = path.join(outputPath, destFileMapName)
+
+      let cssOutput = result.css
+      if (conf.sourceMap) {
+        cssOutput += `\n/*# sourceMappingURL=${destFileMapName} */`
+      }
+
+      await mkdirp(outputPath)
+      eleventyConfig.logger.log(`Writing ${destFilePath} from ${sourcePath} (sass)`)
+      await fs.writeFile(destFilePath, cssOutput)
+
+      if (conf.sourceMap) {
+        eleventyConfig.logger.log(`Writing ${destFileMapPath} from ${sourcePath} (sass)`)
+        const fileMapOutput = JSON.stringify(result.sourceMap)
+        await fs.writeFile(destFileMapPath, fileMapOutput)
+      }
+
+      processed.set(sourcePath, destFileUrl)
+      return destFileUrl
+    } catch (err) {
+      const getStackTrace = function () {
+        const obj = {}
+        Error.captureStackTrace(obj, getStackTrace)
+        return obj.stack
+      }
+      eleventyConfig.logger.error(`${err.toString()}\n${getStackTrace()}`)
     }
-
-    const loadPaths = [
-      eleventyConfig.dir.input,
-      eleventyConfig.dir.includes || '_includes',
-      ...conf.includePaths
-    ]
-
-    const result = await sass.compileAsync(sourcePath, {
-      style: conf.minify ? 'compressed' : 'expanded',
-      sourceMap: conf.sourceMap,
-      sourceMapIncludeSources: conf.sourceMapIncludeSources,
-      loadPaths
-    })
-
-    const outputPath = path.join(eleventyConfig.dir.output, conf.outputPath)
-    const fileHash = crypto.createHash('md5').update(result.css).digest('hex').substring(0, 8)
-    const destFileName = `${path.basename(filePath, '.scss')}.${fileHash}.css`
-    const destFileMapName = `${destFileName}.map`
-    const destFilePath = path.join(outputPath, destFileName)
-    const destFileUrl = `/${conf.outputPath}/${destFileName}`
-    const destFileMapPath = path.join(outputPath, destFileMapName)
-
-    let cssOutput = result.css
-    if (conf.sourceMap) {
-      cssOutput += `\n/*# sourceMappingURL=${destFileMapName} */`
-    }
-
-    await mkdirp(outputPath)
-    eleventyConfig.logger.log(`Writing ${destFilePath} from ${sourcePath} (sass)`)
-    await fs.writeFile(destFilePath, cssOutput)
-
-    if (conf.sourceMap) {
-      eleventyConfig.logger.log(`Writing ${destFileMapPath} from ${sourcePath} (sass)`)
-      const fileMapOutput = JSON.stringify(result.sourceMap)
-      await fs.writeFile(destFileMapPath, fileMapOutput)
-    }
-
-    processed.set(sourcePath, destFileUrl)
-    return destFileUrl
   })
 }
