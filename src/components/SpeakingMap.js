56688c2edc68dc860881d544acd0c23347373d4e
Luciano Mammino <lucianomammino@gmail.com>
2023-07-02 13:12:45 +0100

    Restored map in speaking section

--- src/components/SpeakingMap.js
+++ src/components/SpeakingMap.js
@@ -28,17 +28,13 @@ class SpeakingMap extends Component {
   }
 
   createMap () {
-    const StamenWatercolor = L.tileLayer(
-      'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}',
-      {
-        attribution:
-          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        subdomains: 'abcd',
-        minZoom: 1,
-        maxZoom: 16,
-        ext: 'png'
-      }
-    )
+    const StamenWatercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 1,
+      maxZoom: 16,
+      ext: 'jpg'
+    })
 
     const mymap = L.map('speaking_map').setView([51.505, -0.09], 3)
     StamenWatercolor.addTo(mymap)
