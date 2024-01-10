2baf4ab1a3c91d0ae251ba4310389a5dba3d5791
Luciano Mammino <lucianomammino@gmail.com>
2024-01-10 19:19:31 +0000

    Switched to pnpm and added tailwind

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,13 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import { defineConfig } from 'astro/config'
-import { SITE_URL } from './src/consts'
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import { SITE_URL } from './src/consts';
+
+// biome-ignore lint/nursery/noDefaultExport: expected
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-// biome-ignore lint/nursery/noDefaultExport: expected
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap({})],
-})
+  integrations: [mdx(), sitemap({}), tailwind()]
+});
