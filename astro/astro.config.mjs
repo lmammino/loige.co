016df5ffe872f3d2e951272cf1116c69a445c061
Luciano Mammino <lucianomammino@gmail.com>
2024-01-13 22:21:14 +0000

    Good progress on blog indexes

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,11 +3,18 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import { SITE_URL } from './src/consts';
 
-// biome-ignore lint/nursery/noDefaultExport: expected
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap({}), tailwind()]
+  integrations: [mdx(), sitemap({}), tailwind()],
+  redirects: {
+    '/page/2': '/blog/page/2',
+    '/page/3': '/blog/page/3',
+    '/page/4': '/blog/page/4',
+    '/page/5': '/blog/page/5',
+    '/page/6': '/blog/page/6',
+    '/page/7': '/blog/page/7',
+  }
 });
