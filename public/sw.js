abc76bccce3c37061366a8457c676193c76b0a8b
Luciano Mammino <lucianomammino@gmail.com>
2024-01-21 13:06:48 +0000

    Updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,17 @@
 // this service worker doesn't do anything intentionally.
 // It's just here to clear up the previously installed SW from Gatsby
-self.addEventListener('install', _event => {
-  // do nothing
+// based on https://github.com/NekR/self-destroying-sw
+self.addEventListener('install', (_e) => {
+  self.skipWaiting()
 })
-self.addEventListener('activate', _event => {
-  // do nothing
+
+self.addEventListener('activate', (_e) => {
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      for (const client of clients) {
+        client.navigate(client.url)
+      }
+    })
 })
