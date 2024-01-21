e93899586c5d2c18cb8548afc28231225e0aba69
Luciano Mammino <lucianomammino@gmail.com>
2024-01-21 11:09:42 +0000

    Added service worker cleanup and cleaned up pre-loading of unused fonts

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// this service worker doesn't do anything intentionally.
+// It's just here to clear up the previously installed SW from Gatsby
+self.addEventListener('install', _event => {
+  // do nothing
+})
+self.addEventListener('activate', _event => {
+  // do nothing
+})
